6854863940f9a7ce7436bcb340a88d2b550a8a0c
Thomas Van Kerckvoorde <thomas.vankerckvoorde@clarabridge.com>
2021-04-09 11:46:35 +0200

    ✨ Added Help text component
    Co-authored-by: Cedric <cedric.dhooge@engagor.com>

---
+++ src/stories/HelpText.stories.tsx
@@ -0,0 +1,27 @@
+import { Meta, Story } from '@storybook/react/types-6-0';
+import React from 'react';
+
+import HelpText, {HelpTextProps} from "../help/HelpText";
+import '../css/tooltip.css';
+
+export default {
+    title: 'HelpText',
+    component: HelpText,
+} as Meta;
+
+const Template: Story<HelpTextProps> = (args) =>
+    <HelpText {...args}>
+        {args.children}
+    </HelpText>
+
+export const HelpTextOnIcon = Template.bind({});
+export const HelpTextOnContent = Template.bind({});
+
+HelpTextOnIcon.args = {
+    text: 'Help text content',
+};
+
+HelpTextOnContent.args = {
+    text: 'Help text content',
+    children: <div>Hello</div>
+};
