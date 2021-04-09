0d9b86c3e5a119f3018c210d378fe8f55d40ddc7
Thomas Van Kerckvoorde <thomas.vankerckvoorde@clarabridge.com>
2021-04-09 15:24:29 +0200

    ✨ Add link button
    Co-authored-by: Cedric <cedric.dhooge@engagor.com>

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,11 @@ import './css/components.css';
 import Button from './buttons/Button';
 import ButtonGroup from './buttons/ButtonGroup';
 import SwitchButton from './buttons/SwitchButton';
+import LinkButton from './buttons/LinkButton';
 
 export {
     Button,
     SwitchButton,
-    ButtonGroup
+    ButtonGroup,
+    LinkButton
 };
---
+++ src/stories/LinkButton.stories.tsx
@@ -0,0 +1,30 @@
+import { Meta, Story } from '@storybook/react/types-6-0';
+import React from 'react';
+
+import '../css/components.css';
+import LinkButton, {LinkButtonProps} from "../buttons/LinkButton";
+
+export default {
+    title: 'LinkButton',
+    component: LinkButton,
+    args: {
+        mode: 'primary',
+        size: 'normal',
+    },
+} as Meta;
+
+const TemplateButton: Story<LinkButtonProps> = (args) => <LinkButton {...args}>Button</LinkButton>;
+
+export const Default = TemplateButton.bind({});
+export const Primary = TemplateButton.bind({});
+export const Secondary = TemplateButton.bind({});
+export const Danger = TemplateButton.bind({});
+
+export const Small = TemplateButton.bind({});
+
+Default.args = { mode: 'default' };
+Primary.args = { mode: 'primary' };
+Secondary.args = { mode: 'secondary' };
+Danger.args = { mode: 'danger' }
+
+Small.args = { mode: 'primary', size: 'small' };
