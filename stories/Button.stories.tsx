e19136a54e822b735aaf2d1d5be859c657ebbbfc
Cédric D'hooge <cedric.dhooge@engagor.com>
2021-07-02 18:24:39 +0200

    ✨ Add warning to storybook

--- stories/Button.stories.tsx
+++ stories/Button.stories.tsx
@@ -22,6 +22,7 @@ const TemplateButtonGroup: Story<ButtonGroupProps> = (args) => <ButtonGroup><But
 export const Default = TemplateButton.bind({});
 export const Primary = TemplateButton.bind({});
 export const Secondary = TemplateButton.bind({});
+export const Warning = TemplateButton.bind({});
 export const Danger = TemplateButton.bind({});
 
 export const Small = TemplateButton.bind({});
@@ -31,6 +32,7 @@ export const Group = TemplateButtonGroup.bind({});
 Default.args = { };
 Primary.args = { color: 'primary' };
 Secondary.args = { mode: 'secondary' };
+Warning.args = { color: 'warning' }
 Danger.args = { color: 'danger' }
 
 Small.args = { mode: 'primary', size: 'small' };
