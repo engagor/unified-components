e7d59332acd643dbbe852609d063fd9c71f22f98
Cédric D'hooge <cedric.dhooge@engagor.com>
2021-07-02 12:36:43 +0200

    🐛 Fix purger to include `--` params

--- webpack.config.js
+++ webpack.config.js
@@ -43,7 +43,7 @@ module.exports = {
                 }
             ),
             safelist: {
-                greedy: [/--\w*$/]
+                greedy: [/--[\w\-]*$/]
             }
         }),
     ],
