db3b02c721ac106fd06282c286a250a2adc35ff8
Cédric D'hooge <cedric.dhooge@engagor.com>
2021-07-02 12:10:36 +0200

    ✨ Implement new mode and colors for buttons

--- src/buttons/Button.tsx
+++ src/buttons/Button.tsx
@@ -1,34 +1,50 @@
 import '@clarabridge/unified-ui/dist/buttons.css';
-import React, { PropsWithChildren, ReactElement } from 'react';
+import React, { PropsWithChildren, ReactElement, MouseEvent } from 'react';
 
 export type ButtonProps = {
     mode?: 'primary' | 'secondary';
     color?: 'charcoal' | 'primary' | 'warning' | 'danger';
     size?: 'normal' | 'small';
     className?: string;
-    autoFocus?: boolean;
     disabled?: boolean;
     name?: string;
     type: 'submit' | 'reset' | 'button';
     value?: string | ReadonlyArray<string> | number;
-    onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
+    onClick?: (event: MouseEvent<HTMLButtonElement>) => void;
 };
 
 export default function Button (props: PropsWithChildren<ButtonProps>): ReactElement {
     const mode = props.mode ?? 'primary';
     const color = props.color ?? 'charcoal';
-    const size = props.size ?? 'normal';
 
-    const buttonClasses = ['btn', `btn--size-${size}`, `btn--mode-${mode}`, `btn--color-${color}`];
+    const buttonClasses = ['btn', `btn--mode-${mode}`, `btn--color-${color}`];
 
     if (props.disabled) {
         buttonClasses.push('btn--disabled');
     }
 
+    if (props.size === 'small') {
+        buttonClasses.push('btn--small');
+    }
+
+    const handleClickedButton = (event: MouseEvent<HTMLButtonElement>) => {
+        if (props.disabled) {
+            return;
+        }
+
+        if (props.onClick) {
+            props.onClick(event);
+        }
+    };
+
     return (
         <button
-            {...props}
             className={`${buttonClasses.join(' ')} ${props.className || ''}`}
+            disabled={props.disabled}
+            name={props.name}
+            type={props.type}
+            value={props.value}
+            onClick={handleClickedButton}
         >
             {props.children}
         </button>
