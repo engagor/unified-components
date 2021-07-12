f849c0524275d90579c06915cccee0e0e176e99d
Thomas Van Kerckvoorde <thomas.vankerckvoorde@clarabridge.com>
2021-07-12 09:07:37 +0200

    👌 Linting fixes + renamed file

--- src/elements/Divider.tsx
+++ src/elements/Divider.tsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { ReactElement } from 'react';
 
 export type DividerProps = {
     color?: string;
     className?: string;
 };
 
-export default function Divider(props: DividerProps) {
+export default function Divider(props: DividerProps): ReactElement {
     return <hr className={`border-0 border-b-1 border-solid ${props.color ?? 'border-charcoal-color-200'} ${props.className}`} />
 }
