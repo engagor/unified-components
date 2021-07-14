2654e454b0d3e5e58904751b8ea918030f12fedb
Cédric D'hooge <cedric.dhooge@engagor.com>
2021-07-14 11:13:57 +0200

    🐛 Define parent libraries as peer-dependencies
    To avoid duplicate React modules from being loaded we should define them as
    peer dependencies and externals.

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,22 @@ module.exports = {
     resolve: {
         extensions: ['.ts', '.tsx', '.js', '.css']
     },
+    externals: {
+        react: {
+            root: 'React',
+            commonjs2: 'react',
+            commonjs: 'react',
+            amd: 'react',
+            umd: 'react',
+        },
+        'react-dom': {
+            root: 'ReactDOM',
+            commonjs2: 'react-dom',
+            commonjs: 'react-dom',
+            amd: 'react-dom',
+            umd: 'react-dom',
+        },
+    },
     module: {
         rules: [
             {
